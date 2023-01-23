8c98738fdb36db8348b12d61db25710b3a7d21d4
MOHAN SAI DINESH BODDAPATI <85325733+mohansaidinesh@users.noreply.github.com>
2023-01-23 18:16:28 +0530

    Update UniqueElementIndex.java

--- 23-01-2023/UniqueElementIndex.java
+++ 23-01-2023/UniqueElementIndex.java
@@ -1,3 +1,9 @@
+/* Given a string s, find the first non-repeating character in it and return its index. If it does not exist, return -1.
+
+Example 1:
+Input: s = "leetcode"
+Output: 0 */
+
 class Solution {
     public int firstUniqChar(String s) {
         HashMap<Character,Integer>map=new HashMap<>();
