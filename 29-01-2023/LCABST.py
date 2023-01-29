adb64da3086ed8ec77e023c2e35e8e458d0eceea
MOHAN SAI DINESH BODDAPATI <85325733+mohansaidinesh@users.noreply.github.com>
2023-01-29 21:57:56 +0530

    Update LCABST.py

--- 29-01-2023/LCABST.py
+++ 29-01-2023/LCABST.py
@@ -1,4 +1,13 @@
-if root==None:
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
+        if root==None:
             return None
         if root.val==p.val or root.val==q.val:
             return root
