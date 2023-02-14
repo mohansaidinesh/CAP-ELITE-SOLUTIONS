8c61ebc075905d995cecd2c689ba2d566c4d313e
MOHAN SAI DINESH BODDAPATI <85325733+mohansaidinesh@users.noreply.github.com>
2023-02-14 14:02:04 +0530

    Create minTimeSpoil.py

---
+++ 13-02-2023/minTimeSpoil.py
@@ -0,0 +1,31 @@
+def rotten(mat):
+    r=len(mat)
+    c=len(mat[0])
+    fc=0
+    rt=[]
+    for i in range(r):
+        for j in range(c):
+            if mat[i][j]==2:
+                rt.append((i,j))
+            elif mat[i][j]==1:
+                fc+=1
+    mins=0
+    l=[(0,-1),(-1,0),(0,1),(1,0)]
+    while rt and fc >0:
+        mins+=1
+        for _ in range(len(rt)):
+            x,y =rt.pop(0)
+            for m,n in l:
+                nx,ny=x+m,y+n
+                if nx < 0 or nx == r or ny < 0 or ny == c:
+                            continue
+                if mat[nx][ny]==0 or mat[nx][ny]==2:
+                    continue
+                fc-=1
+                mat[nx][ny]=2
+                rt.append((nx,ny))
+    return mins if fc==0 else -1
+
+l=[int(x) for x in input().split()]
+mat = [list(map(int,input().split())) for i in range(l[0])] 
+print(rotten(mat))
